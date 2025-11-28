accb0747f93c8dc4b2b7b06a1e02fad24f91ce00
sergioluis2803 <sergiomv2803@gmail.com>
2025-11-28 12:46:12 -0500

    Feature/Update-lib Actualizacion de libreria y gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.icons)
 
     //Navigation
     implementation(libs.androidx.navigation.compose)
